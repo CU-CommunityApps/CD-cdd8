f17f9c942495782c85dec49e0922c9489c71ab22
Nick Tubbs <nt328@cornell.edu>
2017-06-28 18:47:46 +0000

    Environment switch around the saml lines of settings.php

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -25,7 +25,8 @@ if (file_exists($local_settings)) {
 }
 $settings['install_profile'] = 'cubear_ip';
 
-
+if (defined('PANTHEON_ENVIRONMENT')){
 # Provide universal absolute path to the installation.
 $ps = json_decode($_SERVER['PRESSFLOW_SETTINGS'], TRUE);
 $settings['simplesamlphp_dir'] = $_ENV['HOME'] .'/code/private/simplesamlphp-1.14.2';
+}
